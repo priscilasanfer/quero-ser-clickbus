8d8faaade3365ee55cfec210add7d651da978d9d
vanessa.costa <vanessa.costa@wirecard.com>
2020-01-11 17:54:45 -0300

    creating a project using spring initializr

---
+++ testes/backend-developer/solution/places-management/src/main/java/com/clickbus/challenge/placesmanagement/PlacesManagementApplication.java
@@ -0,0 +1,13 @@
+package com.clickbus.challenge.placesmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacesManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlacesManagementApplication.class, args);
+    }
+
+}
---
+++ testes/backend-developer/solution/places-management/src/test/java/com/clickbus/challenge/placesmanagement/PlacesManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clickbus.challenge.placesmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacesManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
