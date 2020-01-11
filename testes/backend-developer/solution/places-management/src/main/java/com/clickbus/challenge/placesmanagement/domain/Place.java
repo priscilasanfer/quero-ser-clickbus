db7e5bfb19ba7e57e553f5b53d0cca15556d910f
vanessa.costa <vanessa.costa@wirecard.com>
2020-01-11 18:24:28 -0300

    add validation annotations on domain

--- testes/backend-developer/solution/places-management/src/main/java/com/clickbus/challenge/placesmanagement/domain/Place.java
+++ testes/backend-developer/solution/places-management/src/main/java/com/clickbus/challenge/placesmanagement/domain/Place.java
@@ -3,13 +3,20 @@ package com.clickbus.challenge.placesmanagement.domain;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Builder
@@ -21,10 +28,24 @@ public class Place {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @NotNull(message = "Name cannot be null")
+    @Size(min = 2, message = "Name must not be less than 2 characters")
     private String name;
+
+    @NotNull(message = "Slug cannot be null")
+    @Size(min = 2, message = "Slug must not be less than 2 characters")
     private String slug;
+
+    @NotNull(message = "City cannot be null")
+    @ManyToOne
+    @JoinColumn(name = "city_id", nullable = false)
     private City city;
-    private State state;
+
+    @Column(nullable = false, updatable = false)
+    @CreatedDate
     private Date createdAt;
+
+    @LastModifiedDate
     private Date updatedAt;
 }
--- testes/backend-developer/solution/places-management/src/main/java/com/clickbus/challenge/placesmanagement/domain/State.java
+++ testes/backend-developer/solution/places-management/src/main/java/com/clickbus/challenge/placesmanagement/domain/State.java
@@ -3,13 +3,18 @@ package com.clickbus.challenge.placesmanagement.domain;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Builder
@@ -21,9 +26,20 @@ public class State {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @NotNull(message = "Name cannot be null")
+    @Size(min = 2, message = "Name must not be less than 2 characters")
     private String name;
-    private String postalCode;
+
+    @NotNull(message = "Abbreviation cannot be null")
+    @Size(min = 2, max = 3, message = "Abbreviation must be equal to 2 characters\")")
+    private String abbreviation;
+
+    @Column(nullable = false, updatable = false)
+    @CreatedDate
     private Date createdAt;
+
+    @LastModifiedDate
     private Date updatedAt;
 
 }
