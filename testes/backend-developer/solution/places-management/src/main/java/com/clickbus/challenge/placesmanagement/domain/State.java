924d467144df9a0bcbd478393a85c4dacd291425
vanessa.costa <vanessa.costa@wirecard.com>
2020-01-11 18:07:56 -0300

    create domain

---
+++ testes/backend-developer/solution/places-management/src/main/java/com/clickbus/challenge/placesmanagement/domain/State.java
@@ -0,0 +1,29 @@
+package com.clickbus.challenge.placesmanagement.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class State {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String postalCode;
+    private Date createdAt;
+    private Date updatedAt;
+
+}
