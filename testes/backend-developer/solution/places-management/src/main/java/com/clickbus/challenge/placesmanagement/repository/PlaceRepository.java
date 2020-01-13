59af4884ab01e1d6fe8f0d2c90a5ae34ddd42279
vanessa.costa <vanessa.costa@wirecard.com>
2020-01-12 23:07:02 -0300

    changing method to search for all places by name like ignore case

--- testes/backend-developer/solution/places-management/src/main/java/com/clickbus/challenge/placesmanagement/repository/PlaceRepository.java
+++ testes/backend-developer/solution/places-management/src/main/java/com/clickbus/challenge/placesmanagement/repository/PlaceRepository.java
@@ -8,5 +8,6 @@ import java.util.List;
 
 @Repository
 public interface PlaceRepository extends JpaRepository<Place, Long> {
-    List<Place> findByName(String name);
+
+    List<Place> findByNameContainingIgnoreCase(String name);
 }
