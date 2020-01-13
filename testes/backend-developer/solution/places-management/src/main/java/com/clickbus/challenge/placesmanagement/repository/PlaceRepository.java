8facc6cc70a90834d4c02f85a7389e96a2d8901a
vanessa.costa <vanessa.costa@wirecard.com>
2020-01-12 21:21:01 -0300

    implement PlaceService and add findByName in the PlaceRepository

--- testes/backend-developer/solution/places-management/src/main/java/com/clickbus/challenge/placesmanagement/repository/PlaceRepository.java
+++ testes/backend-developer/solution/places-management/src/main/java/com/clickbus/challenge/placesmanagement/repository/PlaceRepository.java
@@ -4,7 +4,9 @@ import com.clickbus.challenge.placesmanagement.domain.Place;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PlaceRepository extends JpaRepository<Place, Long> {
-
+    List<Place> findByName(String name);
 }
--- testes/backend-developer/solution/places-management/src/main/java/com/clickbus/challenge/placesmanagement/service/PlaceService.java
+++ testes/backend-developer/solution/places-management/src/main/java/com/clickbus/challenge/placesmanagement/service/PlaceService.java
@@ -1,41 +1,76 @@
 package com.clickbus.challenge.placesmanagement.service;
 
-import com.clickbus.challenge.placesmanagement.domain.City;
-import com.clickbus.challenge.placesmanagement.dto.request.CityRequest;
-import com.clickbus.challenge.placesmanagement.dto.response.CityResponse;
-import com.clickbus.challenge.placesmanagement.mapper.CityMapper;
+import com.clickbus.challenge.placesmanagement.domain.Place;
+import com.clickbus.challenge.placesmanagement.dto.request.PlaceRequest;
+import com.clickbus.challenge.placesmanagement.dto.response.PlaceResponse;
+import com.clickbus.challenge.placesmanagement.mapper.PlaceMapper;
 import com.clickbus.challenge.placesmanagement.repository.CityRepository;
-import com.clickbus.challenge.placesmanagement.repository.StateRepository;
+import com.clickbus.challenge.placesmanagement.repository.PlaceRepository;
+import com.github.slugify.Slugify;
 import lombok.AllArgsConstructor;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @AllArgsConstructor
-public class CityService {
+public class PlaceService {
 
+    private final PlaceRepository placeRepository;
     private final CityRepository cityRepository;
-    private final StateRepository stateRepository;
 
-    public CityResponse create(CityRequest cityRequest) {
-        City city = City.builder()
-                .name(cityRequest.getName())
-                .state(stateRepository
-                        .findById(cityRequest.getStateId())
+    private final Slugify slugify = new Slugify();
+
+    public PlaceResponse create(PlaceRequest placeRequest) {
+        Place place = Place.builder()
+                .name(placeRequest.getName())
+                .slug(slugify.slugify(placeRequest.getName()))
+                .city(cityRepository
+                        .findById(placeRequest.getCityId())
                         .orElseThrow(ResourceNotFoundException::new))
                 .build();
 
-        city = cityRepository.save(city);
+        place = placeRepository.save(place);
 
-        return CityMapper.INSTANCE.cityToCityResponse(city);
+        return PlaceMapper.INSTANCE.placeToPlaceResponse(place);
     }
 
-    public CityResponse findById(Long id) {
-        City city = cityRepository
+    public PlaceResponse findById(Long id) {
+        Place place = placeRepository
                 .findById(id)
                 .orElseThrow(ResourceNotFoundException::new);
 
-        return CityMapper.INSTANCE.cityToCityResponse(city);
+        return PlaceMapper.INSTANCE.placeToPlaceResponse(place);
+    }
+
+    public List<PlaceResponse> findAll() {
+        return placeRepository.findAll().stream()
+                .map(place -> PlaceMapper.INSTANCE.placeToPlaceResponse(place))
+                .collect(Collectors.toList());
+    }
+
+    public List<PlaceResponse> findByName(String name) {
+        return placeRepository.findByName(name).stream()
+                .map(place -> PlaceMapper.INSTANCE.placeToPlaceResponse(place))
+                .collect(Collectors.toList());
+    }
+
+    public PlaceResponse update(Long id, PlaceRequest placeRequest) {
+        Place place = placeRepository
+                .findById(id)
+                .orElseThrow(ResourceNotFoundException::new);
+
+        place.setName(placeRequest.getName());
+        place.setSlug(slugify.slugify(place.getName()));
+        place.setCity(cityRepository
+                .findById(id)
+                .orElseThrow(ResourceNotFoundException::new));
+
+        place = placeRepository.save(place);
+
+        return PlaceMapper.INSTANCE.placeToPlaceResponse(place);
     }
 
 }
